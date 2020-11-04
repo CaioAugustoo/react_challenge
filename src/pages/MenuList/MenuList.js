cd4d5d4178272d47a73705758af2f7fb181e98f7
caioaugustoo <caioamfr@gmail.com>
2020-11-04 01:58:33 -0300

    Uso do componente head

--- src/pages/MenuList/MenuList.js
+++ src/pages/MenuList/MenuList.js
@@ -1,8 +1,14 @@
 import React from "react";
+import Head from "../../components/Helper/Head/Head";
 import Menu from "../../components/Menu/Menu";
 
 const MenuList = () => {
-  return <Menu />;
+  return (
+    <>
+      <Menu />
+      <Head title="Cardápio" />
+    </>
+  );
 };
 
 export default MenuList;
